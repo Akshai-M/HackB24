56b7ef1127c3b218a5d21b82832f889c6bac57c6
Akshai-M <mylapuramakshaikumar@gmail.com>
2024-07-25 18:05:40 +0530

    modified

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,9 @@
-// import { asyncHandler } from "../utils/asyncHandler.js";
-// import { ApiError } from "../utils/ApiError.js"
-// import { User } from "../models/user.model.js"
-// import { ApiResponse } from "../utils/ApiResponse.js";
-// import zod from "zod";
-// import jwt from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
+import zod from "zod";
+import jwt from "jsonwebtoken";
 
 const cookieOptions = {
     secure: process.env.NODE_ENV === 'development' ? true : false,
@@ -11,6 +11,21 @@ const cookieOptions = {
     httpOnly: true,
     secure: true
 }
+
+const generateAccessAndRefereshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+        user.refreshToken = refreshToken
+
+        await user.save({ validateBeforeSave: false })
+        return { accessToken, refreshToken }
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
+
 
 
 export {
